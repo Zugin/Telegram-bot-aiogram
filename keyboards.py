b4e7fc43a27e66d490b32c88ccfc000c8d964779
Notebook <Zugin2009@yandex.ru>
2021-09-29 08:43:01 +0300

    v2

--- keyboards.py
+++ keyboards.py
@@ -4,17 +4,6 @@ from aiogram.types import ReplyKeyboardRemove, \
 
 
 # Инлайн клавиатура
-"""
-inline_kb_full = InlineKeyboardMarkup(row_width=2)
-inline_btn_1 = InlineKeyboardButton('Первая', callback_data='button1')
-inline_btn_2 = InlineKeyboardButton('Вторая', callback_data='button2')
-inline_kb_full.add(inline_btn_1, inline_btn_2)
-inline_btn_3 = InlineKeyboardButton('Третья', callback_data='button3')
-inline_kb_full.row(inline_btn_3)
-inline_kb_full.insert(InlineKeyboardButton("query='qwerty'", switch_inline_query='qwerty'))
-inline_kb_full.insert(InlineKeyboardButton("Inline в этом же чате", switch_inline_query_current_chat='wasd'))
-"""
-
 def kb(num):
     inline_bn = InlineKeyboardMarkup()
     for i in range(num):
